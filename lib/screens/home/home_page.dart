87929a41d81714e03b562676a56c92dc79cb7567
Thanadol Cheewarojnarong <thanadol1444@gmail.com>
2023-09-03 11:58:28 +0700

    Initial commit

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -86,6 +86,7 @@ class _HomePageState extends State<HomePage> {
               ),
               Spacer(),
               _buildButtonPanel(),
+              Spacer(),
             ],
           ),
         ),
